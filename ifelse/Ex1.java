5ad9a51d31ecad0e8218aabba2439d6dd5b04263
prltavares <olunatavares@outlook.com>
2026-03-30 16:05:41 -0300

    Testes e exemplos para if else

---
+++ ifelse/Ex1.java
@@ -0,0 +1,14 @@
+package ifelse;
+
+public class Ex1 {
+public static void main(String[] args) {
+    int salario = 1000;
+    double bonus;
+    if (salario > 1000) {
+        bonus = salario * 0.10;
+    } else {
+        bonus = salario * 0.15;
+    }
+    System.out.println("Bonus: " + bonus);
+}
+}
