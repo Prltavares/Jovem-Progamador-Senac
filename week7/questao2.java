0c207c524c34d590da35f0d39c91e71e2c151e51
prltavares <olunatavare@outlook.com>
2026-04-13 15:22:28 -0300

    Primeira avaliação em grupo No jovem programador

---
+++ week7/questao2.java
@@ -0,0 +1,23 @@
+package week7;
+
+import java.util.Scanner;
+
+public class questao2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int senhaValida = 1234;
+        int senha;
+        int tentativas = 0;
+        do {
+            System.out.print("Digite a senha: ");
+            senha = sc.nextInt();
+            tentativas++;
+            if (senha != senhaValida) {
+                System.out.println("ACESSO NEGADO");
+            }
+        } while (senha != senhaValida);
+        System.out.println("ACESSO PERMITIDO. Tentativas: " + tentativas);
+        sc.close();
+    }
+                                                                                                                                    
+}
---
+++ week7/questao5.java
@@ -0,0 +1,40 @@
+package week7;
+
+
+
+public class questao5 {
+    public static void main(String[] args) {
+ 
+        int cuboA[][][] = new int[10][3][5];
+        int cuboB[][][] = new int[10][3][5];
+        int cuboC[][][] = new int[10][3][5];
+ 
+        int i, j, k;
+ 
+        for(i = 0; i < 10; i++) {
+            for(j = 0; j < 3; j++) {
+                for(k = 0; k < 5; k++) {
+ 
+                    cuboA[i][j][k] = 2;
+                    cuboB[i][j][k] = 3;
+ 
+                    cuboC[i][j][k] = cuboA[i][j][k] + cuboB[i][j][k];
+ 
+                }
+            }
+        }
+ 
+        for(i = 0; i < 10; i++) {
+            System.out.println("Parte " + i);
+ 
+            for(j = 0; j < 3; j++) {
+                for(k = 0; k < 5; k++) {
+                    System.out.print(cuboC[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+ 
+            System.out.println();
+        }
+    }
+}
