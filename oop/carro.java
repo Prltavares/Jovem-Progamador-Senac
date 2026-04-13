a4da2da46acc7b18ce5d0a9d852d369f75941127
prltavares <olunatavare@outlook.com>
2026-04-13 16:50:37 -0300

    update

---
+++ oop/carro.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class carro {
+    public String marca;
+    public String modelo;
+    int nmrpassageiros;
+    double capacidadeCombustivel;
+    double consumoCombustivel;
+}
