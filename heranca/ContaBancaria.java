498d8ee6ddc496ddfe52bc5a6924a78a9ae3f620
prltavares <olunatavares@outlook.com>
2026-05-04 14:56:52 -0300

    update

--- heranca/ContaBancaria.java
+++ heranca/ContaBancaria.java
@@ -12,7 +12,7 @@ public class ContaBancaria {
     public void depositar(double valor) {
         if (valor > 0) {
             this.saldo += valor;
-            System.out.println("Depósito de R$ " + valor + " realizado com sucesso!");
+            System.out.println("O deposito de R$  " + valor + " foi realizado com sucesso!");
         } else {
             System.out.println("Valor de depósito inválido!");
         }
@@ -21,16 +21,16 @@ public class ContaBancaria {
     public void sacar(double valor) {
         if (valor > 0 && valor <= this.saldo) {
             this.saldo -= valor;
-            System.out.println("Saque de R$ " + valor + " realizado com sucesso!");
+            System.out.println(" O saque de R$ " + valor + " foi realizado com sucesso!");
         } else {
-            System.out.println("Saque não permitido! Saldo insuficiente ou valor inválido!");
+            System.out.println(" Saque nao realizado, Saldo insuficiente ou valor inválido!");
         }
     }
 
     public void exibirDados() {
-        System.out.println("\n=== Dados da Conta ===");
-        System.out.println("Nome do Cliente: " + nomeCliente);
-        System.out.println("Número da Conta: " + numConta);
+        System.out.println("\nDados da conta");
+        System.out.println("Nome Cliente: " + nomeCliente);
+        System.out.println("Nmr  conta: " + numConta);
         System.out.println("Saldo: R$ " + String.format("%.2f", saldo));
     }
 
