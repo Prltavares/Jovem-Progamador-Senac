1ed64e79bcdb140c3bf47be5cb06be2ff9f54405
prltavares <olunatavares@outlook.com>
2026-06-11 16:45:29 -0300

    update

---
+++ junit/ContaCorrente.java
@@ -0,0 +1,43 @@
+public class ContaCorrente {
+
+	String numero;
+	String agencia;
+	boolean especial;
+	double limiteEspecial;
+	double valorEspecialUsado;
+	double saldo;
+	String[] situacaoConta = new String[2];
+
+	boolean realizarSaque(double qtdSaque) {
+		if(saldo >= qtdSaque ){ // tem saldo em conta
+			saldo -=qtdSaque;
+			return true;
+		}else { // não tem saldo em conta suficiente
+			if(especial) {// verifica se é especial
+				double limite = limiteEspecial + saldo;
+				if(limite >= qtdSaque) {// verifica se o limite especial tem saldo
+					saldo -=qtdSaque;
+					return true;
+				}else {
+					return false;
+				}
+			}else {// não é especial e não tem saldo suficiente
+				return false;
+			}
+		}
+	}
+
+	void depositar(double valorDepositado) {
+		saldo += valorDepositado;
+	}
+	void consultarSaldo() {
+		int teste = 0;
+		System.out.println("Saldo atual da conta é " + saldo);
+		teste = teste +1;
+
+		}
+	boolean verificarUsoChequeEspecial() {
+		return (saldo < 0);
+	}
+
+}
