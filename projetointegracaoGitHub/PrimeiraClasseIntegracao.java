b55098f39f3e4f442a56089b43cb44010a7085eb
prltavares <olunatavares@outlook.com>
2026-06-17 14:47:15 -0300

    update

---
+++ projetointegracaoGitHub/PrimeiraClasseIntegracao.java
@@ -0,0 +1,5 @@
+public class PrimeiraClasseIntegracao
+    public static void main(String[] args) {
+        System.out.println("Olá, esta é a minha primeira classe de integração com o GitHub!");
+    }
+}
